30c1e764a9258eed75d0d045c044f5bd6e8d6bc8
Evgeny Boiko <dealfree@mail.ru>
2022-10-23 16:54:08 +0000

    V:16

--- 123.py
+++ 123.py
@@ -1,28 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
-import smtplib
-from email.mime.text import MIMEText
-from subprocess import check_output
+import boto3
 
-# Get the git log --stat entry of the new commit
-log = check_output(['git', 'log', '-1', '--stat', 'HEAD'])
-
-# Create a plaintext email message
-msg = MIMEText("Look, I'm actually doing some work:\n\n%s" % log)
-
-msg['Subject'] = 'Git post-commit hook notification'
-msg['From'] = 'dealfree@mail.ru'
-msg['To'] = 'dealfreee@gmail.com'
-
-# Send the message
-SMTP_SERVER = 'smtp.mail.ru'
-SMTP_PORT = 465
-
-session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-session.ehlo()
-session.starttls()
-session.ehlo()
-session.login(msg['From'], 'cdE9g28emWCumxyQLFzK')
-
-session.sendmail(msg['From'], msg['To'], msg.as_string())
-session.quit()
+def publish_to_sns(sub, msg):
+    topic_arn = "arn:aws:sns:us-east-1:371428523361:Commit"
+    sns = boto3.client("sns")
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=msg,
+        Subject=sub
+    )
+publish_to_sns("Commit","This man made a commit")
